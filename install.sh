4bb9dfd3384e2f27317a7fe29d28ce276b1066d6
chendb <kjgfcdb@126.com>
2022-11-03 20:44:59 +0800

    Fix bug in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
+mkdir -p ~/.bin
 cargo build --release
 cp ./target/release/zsh_pinyin_comp ~/.bin/zsh_pinyin_comp
-mkdir -p ~/.bin
 cp ./shell/pinyin-comp.zsh ~/.bin/pinyin-comp.zsh
 echo "source ~/.bin/pinyin-comp.zsh" >> ~/.zshrc
