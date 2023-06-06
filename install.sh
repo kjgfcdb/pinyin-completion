7c9a8c2dc9ed67de3dd1e6fd442151e1a31f7966
chendb <kjgfcdb@126.com>
2023-06-06 09:33:11 +0800

    Optimize install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-mkdir -p ~/.bin
+set -e
 cargo build --release
+mkdir -p ~/.bin
 cp ./target/release/zsh_pinyin_comp ~/.bin/zsh_pinyin_comp
 cp ./shell/pinyin-comp.zsh ~/.bin/pinyin-comp.zsh
 echo "source ~/.bin/pinyin-comp.zsh" >> ~/.zshrc
+echo "export PATH=\$PATH:~/.bin" >> ~/.zshrc
